74b099e073c65a2c1c79064591bde2933afaed13
Felipe Buitrago <flipe282000@gmail.com>
2023-12-08 09:59:40 -0500

    inital song in state to prevent error when not playlist selected

--- src/store/playerStore.js
+++ src/store/playerStore.js
@@ -2,7 +2,18 @@ import { create } from "zustand";
 
 export const usePlayerStore = create((set) => ({
   isPlaying: false,
-  currentMusic: { playlist: null, song: null, songs: [] },
+  currentMusic: { 
+    playlist: {
+      id: 1
+    }, 
+    song:  {
+      id: 1,
+      title: "Moonlit Walk",
+      image: `https://vinyl.lofirecords.com/cdn/shop/products/VINYL_MORNING_COFFEE_4-min.png?v=1680526353`,
+      artists: ["LoFi Dreamer"],
+    }, 
+    songs: [] 
+  },
   volume: 1,
   setVolume: (volume) => set({ volume }),
   setIsPlaying: (isPlaying) => set({ isPlaying }),
